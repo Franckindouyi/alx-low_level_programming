d9c1264ff47393d262fb0e59a61c81d9ff94917e
Franckindouyi <Franckindouyi>
2022-12-08 15:47:50 -0800

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 
 {
 	printf("Size of a char: %lu bute(s)\n", sizeof(char));
-	printf("Size of  an int: %lu bute(s)\n", sizeof(int));
+	printf("Size of an int: %lu bute(s)\n", sizeof(int));
 	printf("Size of a long int: %lu bute(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu bute(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu bute(s)\n", sizeof(float));
